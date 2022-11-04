20804700c1273cc06b2b8cc89d057b72aaf1918c
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2022-11-04 13:54:49 +0200

    add username for twitch

--- methods/twitch.py
+++ methods/twitch.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse
+
 import streamlink
 from streamlink.plugins.twitch import TwitchHLSStream
 
@@ -16,6 +18,13 @@ class Stream:
             return None
 
     @property
+    def _get_username(self) -> str or None:
+        try:
+            return urlparse(self.stream_url).path.split("/")[1]
+        except IndexError:
+            return None
+
+    @property
     def _link_check(self) -> bool:
         return True if "twitch.tv" in self.stream_url else False
 
@@ -23,5 +32,6 @@ class Stream:
     def get(self) -> dict:
         stream = self._get_data
         return {
-            "url": stream.url
+            "url": stream.url,
+            "title": self._get_username
         } if stream else {}
