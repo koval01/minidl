593ce20aa7eb9bb442044bca10a0ff4ed2588f5b
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2022-11-04 20:15:07 +0200

    exception for index route

--- app.py
+++ app.py
@@ -7,11 +7,18 @@ from methods import youtube, twitch
 app = Flask(__name__)
 
 
+@app.route('/')
+def empty():
+    return {}
+
+
 @app.route('/<path:path>')
 def get_video(path):
-    result = None
     try:
-        video_yt_id = youtube.ParserLink(f"{path}?{request.query_string.decode()}").get
+        result = None
+        full_url = f"{path}?{request.query_string.decode()}"
+
+        video_yt_id = youtube.ParserLink(full_url).get
         if video_yt_id:
             result = youtube.Video(video_yt_id).get
 
