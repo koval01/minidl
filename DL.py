faffa7dc63b7799ba61eac7d5873919971597443
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2022-11-06 16:30:34 +0200

    fix host in url for proxy

--- DL.py
+++ DL.py
@@ -45,8 +45,8 @@ class Video:
     def _encrypt_link(self, source_url: str) -> str:
         for host in self.proxy_need_hosts:
             if host in source_url:
-                return "%s%s/%s" % (
-                    request.path.split("/")[1], request.host_url,
+                return "%smedia_proxy/%s" % (
+                    request.host_url,
                     proxy.Proxy(self.secret_key).encrypt_url(source_url)
                 )
 
