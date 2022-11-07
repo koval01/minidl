72f941c6d9e363788cd4ee4c67be507d738b57a4
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2022-11-07 14:20:43 +0200

    remove node info

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 import logging as log
 import os
-import sys
-from time import time
-
-import psutil
-import platform
-import requests
-from flask import Flask, request, jsonify, __version__ as flask_ver
+from flask import Flask, request
 
 import DL
 import proxy
@@ -18,29 +12,6 @@ secret_key = os.getenv("SECRET_KEY").encode()
 
 @app.route('/')
 def empty():
-    return {}
-
-
-@app.route('/node')
-def node():
-    if os.getenv("DEBUG"):
-        load1, load5, load15 = psutil.getloadavg()
-        cpu_usage = lambda load: round((load / os.cpu_count()) * 100)
-
-        return jsonify({
-            "host": request.host,
-            "ip": requests.get("https://ident.me").text,
-            "flask": flask_ver,
-            "python": sys.version,
-            "cpu_load": {
-                "load1": cpu_usage(load1),
-                "load5": cpu_usage(load5),
-                "load15": cpu_usage(load15)
-            },
-            "ram_percent": round(psutil.virtual_memory()[2]),
-            "time": round(time()),
-            "platform": platform.platform()
-        })
     return {}
 
 
