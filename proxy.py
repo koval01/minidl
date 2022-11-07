421d921a86d48ad49c79a297d6ed77ac8bd493d5
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2022-11-07 16:31:49 +0200

    update proxy for rezka

--- proxy.py
+++ proxy.py
@@ -12,7 +12,16 @@ PROXIES = {
     'https': env_proxy
 }
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Sec-Ch-Ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua-Platform': '"macOS"',
+    'Dnt': '1', 'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+    'Sec-Fetch-Site': 'none', 'Sec-Fetch-Mode': 'navigate', 'Sec-Fetch-User': '?1',
+    'Sec-Fetch-Dest': 'document', 'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'uk,en;q=0.9,ru-RU;q=0.8,ru;q=0.7',
 }
 
 
@@ -42,14 +51,13 @@ class Proxy:
             return jsonify({})
 
         headers = {key: value for (key, value) in request.headers if key != 'Host'}
-        headers["User-Agent"] = HEADERS["User-Agent"]
-        del headers["Referer"]
+        headers = {**headers, **HEADERS}
 
         resp = requests.request(
             method=request.method,
             url=url,
             proxies=PROXIES,
-            headers={key: value for (key, value) in request.headers if key != 'Host'},
+            headers=headers,
             data=request.get_data(),
             cookies=request.cookies,
             allow_redirects=False,
