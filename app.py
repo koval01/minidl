72f76c4d1a4028b891cdabbcd554100aa127f4fa
Koval Yaroslav <61350874+koval01@users.noreply.github.com>
2022-11-08 13:24:39 +0200

    log proxies after start

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ log.basicConfig(
      level=log.INFO,
      format="[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s",
      datefmt="%H:%M:%S"
- )
+)
+log.info(proxy.PROXIES)
 
 app = Flask(__name__)
 secret_key = os.getenv("SECRET_KEY").encode()
